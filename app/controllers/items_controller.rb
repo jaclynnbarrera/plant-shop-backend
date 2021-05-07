edfd31911eef6b0980f37c5e180e25e10416ae70
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-05-07 12:04:33 -0400

    adds show method in ItemsController/ItemsAPI

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,17 @@ class ItemsController < ApplicationController
         items = Item.all
         render json: items, except: [:created_at, :updated_at]
     end
+
+    def show
+        item = Item.find_by(id: params[:id])
+        render json: item
+    end
+
+    private
+
+    def item_params
+        params.require(:items).permit(:name, :description, :price, :card_id, :id)
+    end
+    
 end
 
