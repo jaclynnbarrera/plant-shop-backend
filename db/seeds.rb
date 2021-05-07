708d83d588e6af5bc3f3ae69703adcb23236875c
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-05-07 05:31:10 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Cart.create(item_count: 2, total_price: 50.99)
+
+Item.create(name: "Monstera", description: "Nicknamed the “swiss cheese plant”, the Monstera deliciosa is famous for its quirky natural leaf holes.", price: 30.00, cart_id: 1)
+Item.create(name: "Fiddle Leaf Fig", description: "The Fiddle Leaf Fig is famous for its broad, vibrant green leaves with prominent veining. It prefers a stable environment and can be fickle when temps fluctuate", price: 25.00, cart_id: 1)
+Item.create(name: "Snake Plant Laurentii", description: "The Snake Plant Laurentii is a succulent plant characterized by its upright swordlike leaves with vibrant yellow edges", price: 20.00, cart_id: 1)
+
